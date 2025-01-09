57cda3070afdd721d14a79754c2aa1c84536df9e
mahdi0shamlou <shamlouprogramer@gmail.com>
2025-01-09 15:58:17 -0500

    create file db_creator for create and manage tables

--- models.py
+++ models.py
@@ -1,3 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy.orm import relationship
+
 
 db = SQLAlchemy()
+
+class users_amin(db.Model):
+    __tablename__ = 'users_admin'
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(191), unique=True, nullable=False)
+    password = db.Column(db.String(191), nullable=False)
+    status = db.Column(db.Integer, nullable=False) # 0 not active 1 active
+    type = db.Column(db.Integer, nullable=False) # 1 super admin 2 admin 3 karshenas 4 ...
+    name = db.Column(db.String(191), nullable=False)
+    phone = db.Column(db.String(191), nullable=False)
+    address = db.Column(db.Text)
+    email = db.Column(db.String(191), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
