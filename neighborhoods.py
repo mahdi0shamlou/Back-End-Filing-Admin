f621eafb736f20bd854a9b5a1aaefa9ec461d3ac
mahdi0shamlou <shamlouprogramer@gmail.com>
2025-01-11 13:51:29 -0500

    create file neighborhoods

---
+++ neighborhoods.py
@@ -0,0 +1,9 @@
+from flask import request, Blueprint, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+neighborhoods_bp = Blueprint('neighborhoods', __name__)
+
+@neighborhoods_bp.route('/Neighborhoods/List', methods=['POST'])
+@jwt_required()
+def Neighborhoods_List():
+    pass
