85da6f1a66b17e6ee98f836912ab2b03235857bb
mahdi0shamlou <shamlouprogramer@gmail.com>
2025-01-14 07:16:24 -0500

    create and test route add neighbrehood to classifications

--- classifications.py
+++ classifications.py
@@ -1,6 +1,6 @@
 from flask import request, Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import db, Classification, ClassificationTypes, ClassificationNeighborhood, users_admin
+from models import db, Classification, ClassificationTypes, ClassificationNeighborhood, users_admin, Neighborhood
 from datetime import datetime
 
 
@@ -71,10 +71,8 @@ def classification_list():
             'message': f'مشکلی پیش اومده ! ：{str(e)}'
         }), 500
 
-
-
 # Route to create classification
-@classification_bp.route('/Classification/Add', methods=['POST'])
+@classification_bp.route('/Classification/Create', methods=['POST'])
 @jwt_required()
 def classification_create():
     try:
@@ -138,10 +136,52 @@ def classification_delete(classification_id):
         }), 500
 
 # Route to add neighborhoods to classification
-@classification_bp.route('/Classification/Neighborhoods/Add', methods=['POST'])
+@classification_bp.route('/Classification/Neighborhoods/<int:classification_id>/Add', methods=['POST'])
 @jwt_required()
-def classification_add_neighborhoods():
-    pass
+def classification_add_neighborhoods(classification_id):
+    try:
+        current_user = get_jwt_identity()
+        user_phone = current_user['phone']
+
+        admin = users_admin.query.filter_by(phone=user_phone).first()
+
+        if not admin or admin.status != 1:
+            return jsonify({
+                'status': 'error',
+                'message': 'شما دسترسی به این بخش ندارید !'
+            }), 403
+        classification = Classification.query.filter_by(id=classification_id)
+        if not classification.first():
+            return jsonify({'status': 'error', 'message': 'دسته بندی پیدا نشد!'}), 404
+
+        request_data = request.get_json()
+        neighborhood_id = request_data['neighborhood_id']
+
+        neighborhood = Neighborhood.query.filter_by(id=neighborhood_id)
+        if not neighborhood.first():
+            return jsonify({'status': 'error', 'message': 'محله پیدا نشد!'}), 404
+
+        query = ClassificationNeighborhood.query
+        query = query.filter(ClassificationNeighborhood.classifiction_id==classification_id)
+        query = query.filter(ClassificationNeighborhood.neighborhood_id==neighborhood_id)
+
+        if not query.first():
+            new_ClassificationNeighborhood = ClassificationNeighborhood(
+                classifiction_id=classification_id,
+                neighborhood_id=neighborhood_id
+            )
+            db.session.add(new_ClassificationNeighborhood)
+            db.session.commit()
+            return jsonify({'status': 'okay', 'message': 'محله اضافه شد!'}), 200
+        else:
+            return jsonify({'status': 'error', 'message': 'محله اضافه شده بوده است!'}), 404
+
+    except Exception as e:
+        return jsonify({
+            'status': 'error',
+            'message': f'مشکلی پیش اومده ! ：{str(e)}'
+        }), 500
+
 
 # Route to delete neighborhoods from classification
 @classification_bp.route('/Classification/Neighborhoods/Delete', methods=['Delete'])
